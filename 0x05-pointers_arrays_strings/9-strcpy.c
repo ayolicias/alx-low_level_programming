f40cc0a8d259a55a3bdedd3014796681c6ff84d2
ziyanda <ziyandandlamafa47@gmail.com>
2022-09-20 14:00:12 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_array - display array
- * @a:integer
- * @n:integer
- *Return:void
+ * _strcpy - copies  src to  dest.
+ * @dest:char
+ * @src:char
+ * Return:char
  */
-void print_array(int *a, int n)
+char *_strcpy(char *dest, char *src)
 {
 int i;
-for (i = 0; i < n; i++)
+
+i = 0;
+	while (src[i])
 	{
-	printf("%d", a[i]);
-		if (i < (n - 1))
-		{
-		printf(", ");
-		}
+		dest[i] = src[i];
+		i++;
 	}
-	printf("\n");
+dest[i] = '\0';
+return (dest);
 }
