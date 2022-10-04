2d0bf15e34f1c57f996a607c2f64946799353eb9
ziyanda <ziyandandlamafa47@gmail.com>
2022-10-04 16:37:25 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,39 @@
-#include "main.h"
+#include <stdlib.h>
+
 /**
- * free_grid - allocates a grid, make space and free space
- * @grid: takes in width of grid
- * @height: height of grid
- * Return: free grid
- */
-
-void free_grid(int **grid, int height)
+ *  * _realloc - reallocates a memory block using malloc and free.
+ *   * @ptr: pointer to previously allocated memory
+ *    * @old_size: size of allocated space for ptr
+ *     * @new_size: size of newly allocated space
+ *      *
+ *       * Return: pointer to newly allocated memory, or NULL if failure
+ *        */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int i;
+		char *p;
+			unsigned int i, max = new_size;
+				char *oldp = ptr;
 
-	for (i = 0; i < height; i++)
-	{
-		free(grid[i]);
-	}
+					if (ptr == NULL)
+							{
+										p = malloc(new_size);
+												return (p);
+													}
+						else if (new_size == 0)
+								{
+											free(ptr);
+													return (NULL);
+														}
+							else if (new_size == old_size)
+										return (ptr);
 
-	free(grid);
+								p = malloc(new_size);
+									if (p == NULL)
+												return (NULL);
+										if (new_size > old_size)
+													max = old_size;
+											for (i = 0; i < max; i++)
+														p[i] = oldp[i];
+												free(ptr);
+													return (p);
 }
